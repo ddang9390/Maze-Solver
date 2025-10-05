b2a752c1dcb0e688489f036622329ef272222687
Daniel Dang <dnld1ng@yahoo.com>
2025-10-05 17:58:11 -0400

    Fixed issues with tests file

--- tests.py
+++ tests.py
@@ -1,11 +1,14 @@
 import unittest
 
-from maze import Maze
-from graphics import Window
+from Maze.maze import Maze
+from Graphics.graphics import Window
 
 class Tests(unittest.TestCase):
     win = Window(800, 600)
     def test_maze_create_cells(self):
+        """
+        Check if correct number of cells can be made
+        """
         num_cols = 12
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10, self.win)
@@ -15,6 +18,9 @@ class Tests(unittest.TestCase):
         )
 
     def test_large_maze(self):
+        """
+        Check if able to make a large maze
+        """
         num_cols = 6
         num_rows = 100
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10, self.win)
@@ -24,6 +30,9 @@ class Tests(unittest.TestCase):
         )
 
     def test_reset_vist(self):
+        """
+        Check if able to clear 'visited' status of cells
+        """
         num_cols = 6
         num_rows = 100
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10, self.win)
@@ -32,9 +41,6 @@ class Tests(unittest.TestCase):
         for col in m1.cells:
             for cell in col:
                 self.assertEqual(cell.visited, False)
-
-
-
 
 
 if __name__ == "__main__":
